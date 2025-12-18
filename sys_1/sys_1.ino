54bfba0ff2df924acbab630c58912e1282f28c52
김성민 <117885263+EMBKSM@users.noreply.github.com>
2025-12-18 12:16:30 +0900

    Replace SoftwareSerial with Serial communication

--- sys_1/sys_1.ino
+++ sys_1/sys_1.ino
@@ -1,13 +1,9 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(9, 8); // RX: 9, TX: 8
-
 void setup() {
-  mySerial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  mySerial.write('a');
+  Serial.write('a');
 
   delay(10); 
 }
